a408b9e88686b878e8594f22d750712c08607c7b
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-02 10:05:17 -0700

    updating RabbitMQ (Bunny) example as per Carl Hörberg

--- benchmark/client_benchmark.rb
+++ benchmark/client_benchmark.rb
@@ -15,16 +15,19 @@ reply_q1 = SecureRandom.hex
 conn = Bunny.new
 conn.start
 ch = conn.create_channel
-q  = ch.queue('calc')
-x  = ch.default_exchange
+x = ch.default_exchange
+q = ch.queue('calc')
+results = Hash.new { |h,k| h[k] = Queue.new }
 reply_q2 = ch.queue('', exclusive: true)
+reply_q2.subscribe(block: false) do |delivery_info, properties, payload|
+  results[properties[:correlation_id]].push payload
+end
 
 n = 1000
 
-Benchmark.bmbm do |x|
-  x.report("RabbitMQ") do
+Benchmark.bmbm do |b|
+  b.report("RabbitMQ") do
     n.times do
-      x  = ch.default_exchange
       # request id will be used as the name of the return queue
       req_message = {
         'id' => SecureRandom.hex,
@@ -33,29 +36,20 @@ Benchmark.bmbm do |x|
         'params' => [1, 5.1]
       }
 
-      # we'll set this in the block passed to subscribe below
-      response = nil
-
       # send out our request, serialized as JSON
-      x.publish(JSON.generate(req_message), {
+      x.publish(req_message.to_json, {
         correlation_id: req_message['id'],
         reply_to: reply_q2.name,
         routing_key: q.name
       })
-
+      response = results[req_message['id']].pop
+      results.delete req_message['id'] # prevent memory leak
       # subscribe to the return queue in a blocking fashion
-      reply_q2.subscribe(block: true) do |delivery_info, properties, payload|
-        if properties[:correlation_id] == req_message['id']
-          response = payload            # visible via closure
-          delivery_info.consumer.cancel # unblock the consumer
-        end
-      end
-
-      y = "1+5.1=%.1f" % JSON.parse(response)['result'] # 1+5.1=6.1
+      "1+5.1=%.1f" % JSON.parse(response)['result'] # 1+5.1=6.1
     end
   end
 
-  x.report("Redis") do
+  b.report("Redis") do
     n.times do
       req = {
         'id' => SecureRandom.hex,
@@ -70,7 +64,7 @@ Benchmark.bmbm do |x|
     end
   end
 
-  x.report("HTTP") do
+  b.report("HTTP") do
     n.times do
       req = Net::HTTP::Post.new uri.path
       req.body = {
